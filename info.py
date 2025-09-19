3b3b85e4affa7172dfcb178e71ae45b444fe081d
zishananis <zishananis3@gmail.com>
2025-09-19 17:01:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'zishananis_official') # owner us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/filmsexpert2')
 CHNL_LNK = environ.get('CHNL_LNK', 'http://t.me/stark_network07')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Stark_Network07') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/filmsexpert_1') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
